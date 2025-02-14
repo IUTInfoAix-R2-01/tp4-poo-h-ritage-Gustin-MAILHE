9b593d10be9f71748ae1adceeed02916b0439e6e
Gustin <gustin.mailhe@etu.univ-amu.fr>
2025-02-14 09:25:30 +0100

    Creation de la class VeloElec - Gustin-MAILHE

---
+++ src/cycles/VeloElec.java
@@ -0,0 +1,41 @@
+package cycles;
+
+public class VeloElec {
+	private static double DEFAUT_FACTEUR_PUISSANCE_MOTEUR = 15.0;
+	private double facteurPuissanceMoteur;
+	
+
+	public static double getDEFAUT_FACTEUR_PUISSANCE_MOTEUR() {
+		return DEFAUT_FACTEUR_PUISSANCE_MOTEUR;
+	}
+	public static void setDEFAUT_FACTEUR_PUISSANCE_MOTEUR(double dEFAUT_FACTEUR_PUISSANCE_MOTEUR) {
+		DEFAUT_FACTEUR_PUISSANCE_MOTEUR = dEFAUT_FACTEUR_PUISSANCE_MOTEUR;
+	}
+	
+	public double getFacteurPuissanceMoteur() {
+		return facteurPuissanceMoteur;
+	}
+	public void setFacteurPuissanceMoteur(double facteurPuissanceMoteur) {
+		this.facteurPuissanceMoteur = facteurPuissanceMoteur;
+	}
+	
+	
+	
+	// <==== Consturceur ====>
+	public VeloElec() {
+		// TODO Auto-generated constructor stub
+	}
+	public VeloElec() {
+		// TODO Auto-generated constructor stub
+	}
+	public VeloElec() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	// <===== Fonction test ====>
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
